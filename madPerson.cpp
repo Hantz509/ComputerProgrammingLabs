ff4ffe97a14424149d0be9009b94a20a1cc5a146
GUERRIER Hantz Brunet <guerrierhantzbrunet@GUERRIERs-MacBook-Air.local>
2023-12-12 13:34:51 +0800

    First push

---
+++ madPerson.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+int main()
+{
+    const char keyboard[] = {"1234567890-=qwertyuiop[]\asdfghjkl;'zxcvbnm,./"};
+
+    string s;
+    cost int 
+    getline (cin, s);
+
+    for (unsigned int i = 0 ; i < s.size(); i++)
+    {
+        if (s[i] == ' ')
+            cout << " ";
+        else
+            for (unsigned int j = 0 ; j < ; j++)
+            {
+                if  ()
+            }
+    }
+
+    return 0;
+}
